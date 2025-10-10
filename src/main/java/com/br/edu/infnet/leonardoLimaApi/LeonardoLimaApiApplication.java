853b24e359d2ad6b4d1b348b6e6b682eb317b288
lima6615 <leo-queiroz1@outlook.com>
2025-10-10 17:08:26 -0300

    feat: projeto inicial

---
+++ src/main/java/com/br/edu/infnet/leonardoLimaApi/LeonardoLimaApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.edu.infnet.leonardoLimaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoLimaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoLimaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/edu/infnet/leonardoLimaApi/LeonardoLimaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.edu.infnet.leonardoLimaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoLimaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
